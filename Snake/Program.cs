fa0a0e708a4a654736827beb1d39df059371c4e6
GorbenkoDV <gorbenkodv@mail.ru>
2020-04-27 22:38:02 +0300

    Class Point added

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -10,6 +10,12 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+            var p1 = new Point() { X = 1, Y = 3, Symbol = '*' };
+            p1.Draw();
+            var p2 = new Point() { X = 4, Y = 5, Symbol = '#' };
+            p2.Draw();
+
+            Console.ReadKey();
         }
     }
 }
